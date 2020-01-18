cbbcd435a18c5d47fbb01d8d85ab87db5a783dc5
Aaron Yoon <58920301+aaron-yoon@users.noreply.github.com>
2020-01-18 01:30:37 -0800

    Add files via upload

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const t2 = new TimelineMax();
 const t3 = new TimelineMax();
 
 
-t1.fromTo(titlecardpicture, 2, {height: "0%"}, {height: "70%", ease: Power2.easeInOut});
+t1.fromTo(titlecardpicture, 2, {height: "0%"}, {height: "80%", ease: Power2.easeInOut});
 t2.fromTo(titlecardtext, 2, {y: "-100%"}, {y: "0%"});
 t3.fromTo(educationcourses, 1.2, {x: "-100%"}, {x: "0%"});
 
@@ -16,9 +16,6 @@ t3.fromTo(educationcourses, 1.2, {x: "-100%"}, {x: "0%"});
 t1.fromTo(titlecardpicture, 2, {height: "0%"}, {height: "70%", ease: Power2.easeInOut}
 ).fromTo(titlecardpicture, 1.2, {width: "100%"}, {width: "100%", ease: Power2.easeInOut});
 */
-
-
-// Scroll Magic
 
 const about = document.querySelector(".about");
 
